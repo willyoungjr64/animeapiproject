41c9077df237f75528a7b44ca01938a11060d2ab
Willlearn github <will588200@gmail.com>
2023-05-16 19:37:41 -0400

    5/16/2023

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
 const quoteHTML1 = document.getElementById("quote1");
 const quoteHTML2 = document.getElementById("quote2");
 const animeImg = document.getElementById("anime-img");
+const animeURL = document.getElementById("anime-url");
+const animeTitle = document.getElementById("anime-title");
+const animeScore = document.getElementById("anime-score");
+const animeButton = document.getElementById("anime-button");
 
+
+
+//console.log(animeURL.innerHTML);
 async function fetchQuote() {
+
+  let score;
+  let title;
+  let imgUrl;
+  let url;
   const res = await fetch("https://api.jikan.moe/v4/random/anime");
   //   const res2 = await fetch("https://api.kanye.rest");
 
   const json = await res.json();
   //   console.log(randomAnime);
-  console.log(json.data.title);
-  console.log(json.data.images.jpg.image_url);
+  title = json.data.title;
+  //console.log(json.data.images.jpg.image_url);
+  score = json.data.score;
 
-  animeImg.src = json.data.images.jpg.image_url;
+  imgUrl = json.data.images.jpg.image_url;
+  url = json.data.url;
+
+  if (score > 7) {
+    animeScore.innerHTML = `Score: ${score}`;
+  } else {
+    animeScore.innerHTML = `This anime has less than a 7 on MyAnimeList (${score}). 👎 Do not watch.`;
+  }
+  animeImg.src = imgUrl;
+  animeTitle.innerHTML = title;
+
+  animeURL.innerHTML = `<a href="${url}">${title}</a>`;
+
+
+
+  
+  
   //   const json2 = await res2.json();
 
   //   quoteHTML1.innerHTML = `This is an inspirational quote: <br>
